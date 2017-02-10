6a5fa4b8808ca7deca2101f8ef9f3925ea8092af
Kyle Benson <kylewaynebenson@gmail.com>
2017-02-10 12:37:50 -0800

    First time it works lol
    Also included beginning code for being able to offset the shadow

--- CreateDropShadow.py
+++ CreateDropShadow.py
@@ -12,7 +12,7 @@ class DropShadow( object ):
 	def __init__( self ):
 		# Window 'self.w':
 		windowWidth  = 250
-		windowHeight = 150
+		windowHeight = 180
 		windowWidthResize  = 300 # user can resize width by this value
 		windowHeightResize = 0   # user can resize height by this value
 		self.w = vanilla.FloatingWindow(
@@ -23,56 +23,98 @@ class DropShadow( object ):
 			autosaveName = "com.kylewaynebenson.DropShadow.mainwindow" # stores last window position and size
 		)
 		
-		# UI
+		# UI elements:
 		self.w.text_1 = vanilla.TextBox( (15, 10, -15, 30), "Create drop shadow that is this many units removed from original drawing:", sizeStyle='small' )
 
 		self.w.text_2 = vanilla.TextBox( ( 15, 60, 20, 20), "X:", sizeStyle='small' )
-		self.w.xAxis = vanilla.EditText( ( 40, 60-1, 50, 19), "10", sizeStyle='small' )
+		self.w.xAxis = vanilla.EditText( ( 40, 60-1, 50, 19), "-20", sizeStyle='small' )
 		self.w.text_3 = vanilla.TextBox( (-95, 60, 20, 20), "Y:", sizeStyle='small' )
-		self.w.yAxis = vanilla.EditText( (-20-50, 60-1, -20, 19), "10", sizeStyle='small' )
+		self.w.yAxis = vanilla.EditText( (-20-50, 60-1, -20, 19), "-20", sizeStyle='small' )
+
+		# self.w.text_4 = vanilla.TextBox( (15, 90, 42, 20), "Offset:", sizeStyle='small' )
+		# self.w.offset = vanilla.EditText( (62, 90-1, 50, 19), "0", sizeStyle='small' )
 		
-		# Run Button
+		
+		# Run Button:
 		self.w.runButton = vanilla.Button((-130, -20-15, -15, -15), "Create Shadows", sizeStyle='regular', callback=self.DropShadowMain )
 		self.w.setDefaultButton( self.w.runButton )
 		
-		# Open window and focus
+		# Load Settings
+		# if not self.LoadPreferences():
+		# 	print "Note: 'Create Drop Shadow' could not load preferences."
+
+		# Open window and focus on it:
 		self.w.open()
 		self.w.makeKey()
 
+	############
+	# Figure out saving later
+	############
+	# def SavePreferences( self, sender ):
+	# 	try:
+	# 		Glyphs.defaults["com.kylewaynebenson.DropShadow.xAxis"] = self.w.xAxis.get()
+	# 		Glyphs.defaults["com.kylewaynebenson.DropShadow.yAxis"] = self.w.yAxis.get()
+	# 	except:
+	# 		return False
+			
+	# 	return True
+
+	# def LoadPreferences( self ):
+	# 	try:
+	# 		self.w.xAxis.set( Glyphs.defaults["com.kylewaynebenson.DropShadow.xAxis"] )
+	# 		self.w.yAxis.set( Glyphs.defaults["com.kylewaynebenson.DropShadow.yAxis"] )
+	# 	except:
+	# 		return False
+			
+	# 	return True
+
+
 	def DropShadowMain( self, sender ):
-	def DropShadowMain( self, sender ):
+		self.offsetCurveFilter = NSClassFromString("GlyphsFilterOffsetCurve")
 		Font = Glyphs.font
 		selectedLayers = Font.selectedLayers
 		xAxis = float(self.w.xAxis.get())
 		yAxis = float(self.w.yAxis.get())
-		shouldBeOdd = False
-		if yAxis < 0:
-			shouldBeOdd = True
+		# offsetX = float(self.w.offset.get())
+		# offsetY = float(self.w.offset.get())
+		glyphsChanged = []
 		try:
+			# offsetLayer = []
 			for thisLayer in selectedLayers:
-				newPathList = []
+
+				glyphsChanged.append( thisLayer.parent.name )
+				thisLayer.parent.beginUndo() # wrapper for undo function
+
+				thisLayer.correctPathDirection() # double counter (B and 8) get missed here?
+				thisLayer.correctPathDirection() # single counters (D O) get bad now too
+				thisLayer.correctPathDirection() # for some reason now everything is good
+				addPathList = []
+				negPathList = []
 				for thisPath in thisLayer.paths:
 					newPath = thisPath.copy()
 					for thisNode in newPath.nodes:
 						thisNode.x += xAxis
 						thisNode.y += yAxis
-					newPath.reverse()
-					newPathList.append( newPath )
-				thisLayer.paths.extend( newPathList )
+					negPathList.append( thisPath )
+					addPathList.append( newPath )
+				thisLayer.paths.extend( addPathList )
 				thisLayer.removeOverlap()
-				counter = 0
-				delList = []
-				for thisPath in thisLayer.paths:
-					if shouldBeOdd:
-						if counter % 2 == 0:
-							delList.append( thisPath )
-					else:
-						if counter % 2 != 0:
-							delList.append( thisPath )
-					counter += 1
-				thisLayer.paths = list(set(thisLayer.paths).difference(set(delList)))
+				############
+				# offset doesn't work because you can't use this filter on a python array. Must be a font object
+				############
+				# self.offsetCurveFilter.offsetLayer_offsetX_offsetY_makeStroke_position_error_shadow_( negPathList, offsetX*0.5, offsetY*0.5, True, 0.5, None, None )
+				for thisPath in negPathList:
+					thisPath.reverse()
+				thisLayer.paths.extend( negPathList )
+				thisLayer.removeOverlap()
+				thisLayer.correctPathDirection()
+
+				thisLayer.parent.endUndo() # wrapper for undo function
+
+			print "Created drop shadow for these glyphs:", glyphsChanged
+
 		except Exception, e:
-			# brings macro window to front and reports error:
+			# print error
 			Glyphs.showMacroWindow()
 			print "Create Drop Shadow Error: %s" % e
 
