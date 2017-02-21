1eca4566ec64b60b8e23155025e19512437ef248
Kyle Benson <kylewaynebenson@gmail.com>
2017-02-21 14:52:08 -0800

    Update CreateDropShadow.py
    lol actually works now

--- CreateDropShadow.py
+++ CreateDropShadow.py
@@ -1,8 +1,8 @@
-#MenuTitle: Create Drop Shadow
+#MenuTitle: Create Sign Painter Drop Shadow
 #Created by Kyle Wayne Benson
 # -*- coding: utf-8 -*-
 __doc__="""
-Replace each selected glyphs with a drop shadow
+Replace each selected glyphs with a sign painter style drop shadow
 """
 
 import vanilla
@@ -25,55 +25,39 @@ class DropShadow( object ):
 		)
 		
 		# UI elements:
-		self.w.text_1 = vanilla.TextBox( (15, 10, -15, 30), "Create drop shadow that is this many units removed from original drawing:", sizeStyle='small' )
-
-		self.w.text_2 = vanilla.TextBox( ( 15, 60, 20, 20), "X:", sizeStyle='regular' )
-		self.w.xAxis = vanilla.EditText( ( 40, 60-1, 50, 21), "-20", sizeStyle='regular' )
-		self.w.text_3 = vanilla.TextBox( (-95, 60, 20, 20), "Y:", sizeStyle='regular' )
-		self.w.yAxis = vanilla.EditText( (-20-50, 60-1, -20, 21), "-20", sizeStyle='regular' )
-
-		self.w.text_4 = vanilla.TextBox( (15, 90, 42, 20), "Offset:", sizeStyle='regular' )
-		self.w.offset = vanilla.EditText( (62, 90-1, 50, 21), "0", sizeStyle='regular' )
-
-		self.w.checkBox = CheckBox((-110, 90, 0, 20), "Keep letters", value=False)
+		YOffset = 10
 		
+		self.w.text_1 = vanilla.TextBox( (15, YOffset, -15, 30), "Only use :", sizeStyle='small' )
+		
+		LineHeight = 40
+		YOffset += LineHeight
+		
+		self.w.text_2 = vanilla.TextBox( ( 15, YOffset, 20, 20), "X:", sizeStyle='regular' )
+		self.w.xAxis = vanilla.EditText( ( 40, YOffset, 50, 21), "-20", sizeStyle='regular' )
+		self.w.text_3 = vanilla.TextBox( (-95, YOffset, 20, 20), "Y:", sizeStyle='regular' )
+		self.w.yAxis = vanilla.EditText( (-20-50, YOffset, -20, 21), "-20", sizeStyle='regular' )
+
+		LineHeight = 30
+		YOffset += LineHeight
+
+		self.w.text_4 = vanilla.TextBox( (15, YOffset, 42, 20), "Offset:", sizeStyle='regular' )
+		self.w.offset = vanilla.EditText( (62, YOffset, 50, 21), "0", sizeStyle='regular' )
+		self.w.checkBox = CheckBox((-110, YOffset, 0, 20), "Keep letters", value=False)
+		
+		YOffset += LineHeight
 		
 		# Run Button:
 		self.w.runButton = vanilla.Button((-130, -20-15, -15, -15), "Create Shadows", sizeStyle='regular', callback=self.DropShadowMain )
 		self.w.setDefaultButton( self.w.runButton )
-		
-		# Load Settings
-		# if not self.LoadPreferences():
-		# 	print "Note: 'Create Drop Shadow' could not load preferences."
 
 		# Open window and focus on it:
 		self.w.open()
 		self.w.makeKey()
 
-	############
-	# Figure out saving later
-	############
-	# def SavePreferences( self, sender ):
-	# 	try:
-	# 		Glyphs.defaults["com.kylewaynebenson.DropShadow.xAxis"] = self.w.xAxis.get()
-	# 		Glyphs.defaults["com.kylewaynebenson.DropShadow.yAxis"] = self.w.yAxis.get()
-	# 	except:
-	# 		return False
-			
-	# 	return True
-
-	# def LoadPreferences( self ):
-	# 	try:
-	# 		self.w.xAxis.set( Glyphs.defaults["com.kylewaynebenson.DropShadow.xAxis"] )
-	# 		self.w.yAxis.set( Glyphs.defaults["com.kylewaynebenson.DropShadow.yAxis"] )
-	# 	except:
-	# 		return False
-			
-	# 	return True
-
 
 	def DropShadowMain( self, sender ):
 		self.offsetCurveFilter = NSClassFromString("GlyphsFilterOffsetCurve")
+		pathOp = GSPathOperator.alloc().init()
 		Font = Glyphs.font
 		selectedLayers = Font.selectedLayers
 		xAxis = float(self.w.xAxis.get())
@@ -88,31 +72,49 @@ class DropShadow( object ):
 				glyphsChanged.append( thisLayer.parent.name )
 				thisLayer.parent.beginUndo() # wrapper for undo function
 
-				thisLayer.correctPathDirection() # double counter (B and 8) get missed here?
-				thisLayer.correctPathDirection() # single counters (D O) get bad now too
-				thisLayer.correctPathDirection() # for some reason now everything is good
-				addPathList = []
-				negPathList = []
-				keepPathList = []
+				thisLayer.correctPathDirection() #
+				thisLayer.correctPathDirection() #
+				thisLayer.correctPathDirection() #
+
+				shadowPathList = NSMutableArray.array()
+				prePathList = NSMutableArray.array()
+				
+				#save original outline
 				for thisPath in thisLayer.paths:
-					newPath = thisPath.copy()
-					for thisNode in newPath.nodes:
-						thisNode.x += xAxis
-						thisNode.y += yAxis
-					addPathList.append( newPath )
-					keepPathList.append( thisPath )
-				if (offsetX != 0) & (offsetY != 0):
-					self.offsetCurveFilter.offsetLayer_offsetX_offsetY_makeStroke_position_error_shadow_( thisLayer, offsetX, offsetY, False, 0.5, None, None )
+					newPath = GSPath()
+					for n in thisPath.nodes:
+						newNode = GSNode()
+						setX = n.x + xAxis
+						setY = n.y + yAxis
+						newNode.type = n.type
+						newNode.setPosition_((setX, setY))
+						newPath.addNode_( newNode )
+					newPath.closed = thisPath.closed
+					shadowPathList.append( newPath )
+					prePathList.append( thisPath )
+
+				self.offsetCurveFilter.offsetLayer_offsetX_offsetY_makeStroke_position_error_shadow_( thisLayer, offsetX, offsetY, False, 0.5, None, None )
+
 				for thisPath in thisLayer.paths:
-					negPathList.append( thisPath )
-				thisLayer.paths.extend( addPathList )
-				thisLayer.removeOverlap()
-				for thisPath in negPathList:
+					shadowPathList.append( thisPath )
+
+				pathOp.removeOverlapPaths_error_( shadowPathList, None)
+
+				#reverse shadow
+				for thisPath in thisLayer.paths:
 					thisPath.reverse()
-				thisLayer.paths.extend( negPathList )
+
+				#punch out the old drawing
+				for shadowPath in shadowPathList:
+					thisLayer.addPath_( shadowPath )
+
 				thisLayer.removeOverlap()
+
 				if (self.w.checkBox.get() == True):
-					thisLayer.paths.extend( keepPathList )
+					for prePath in prePathList:
+						thisLayer.addPath_( prePath )
+
+
 				thisLayer.correctPathDirection()
 				thisLayer.parent.endUndo() # wrapper for undo function
 
