5265f41aaac427219a6335d409f1a9224d82e036
Kyle Benson <kylewaynebenson@gmail.com>
2018-01-21 12:41:18 -0800

    adding makenodefirst

---
+++ MakeNodeFirst.py
@@ -0,0 +1,7 @@
+#MenuTitle: Make Node First
+#Created by Kyle Wayne Benson
+#Created this script so that I could assign a keyboard shortcut to this right-click function
+# -*- coding: utf-8 -*-
+sel = Layer.selection
+if len(sel) == 2 and type(sel[0]) == GSNode:
+	sel.makeNodeFirst()
