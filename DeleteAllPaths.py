73e18d4a8969c8c59b7c6db9f90e9f7c53a1f273
rafalbuchner <rafal.buchner@gmail.com>
2021-07-13 13:49:01 +0200

    DeleteAllPaths G3 update

--- DeleteAllPaths.py
+++ DeleteAllPaths.py
@@ -4,7 +4,7 @@
 __doc__="""
 Finds and deletes all paths
 """
-
+from AppKit import NSMutableIndexSet
 import GlyphsApp
 thisFont = Glyphs.font # frontmost font
 thisFontMaster = thisFont.selectedFontMaster # active master
@@ -12,17 +12,32 @@ listOfSelectedLayers = thisFont.selectedLayers # active layers of selected glyph
 
 def deleteSmallestPath( thisLayer ):
 	indexesOfPathsToBeRemoved = []
-	layerarea = []
-	for thisPath in thisLayer.paths:
-		layerarea.append(thisPath.area())
+#	layerarea = []
+#	for thisPath in thisLayer.paths:
+#		layerarea.append(thisPath.area())
+	if Glyphs.versionNumber >= 3:
+		# Glyphs 3 code
+		for index, shape in enumerate(thisLayer.shapes):
+			if shape.shapeType == GSShapeTypePath:
+				indexesOfPathsToBeRemoved.append(index)
+		try:
+			indexes = NSMutableIndexSet.alloc().init()
+			for i in indexesOfPathsToBeRemoved:
+				
+				indexes.addIndex_(i)
+			thisLayer.removeShapesAtIndexes_(indexes)
+		except Exception as e:
+			print(e)
 
-	numberOfPaths = len(thisLayer.paths)
-	for thisPathNumber in range( numberOfPaths ):
-		indexesOfPathsToBeRemoved.append( thisPathNumber )
-	
-	if indexesOfPathsToBeRemoved:
-		for thatIndex in reversed( sorted( indexesOfPathsToBeRemoved )):
-			thisLayer.removePathAtIndex_( thatIndex )
+	else:
+		# Glyphs 2 code
+		numberOfPaths = len(thisLayer.paths)
+		for thisPathNumber in range( numberOfPaths ):
+			indexesOfPathsToBeRemoved.append( thisPathNumber )
+		
+		if indexesOfPathsToBeRemoved:
+			for thatIndex in reversed( sorted( indexesOfPathsToBeRemoved ) ):
+				thisLayer.removePathAtIndex_( thatIndex )
 
 thisFont.disableUpdateInterface() # suppresses UI updates in Font View
 
